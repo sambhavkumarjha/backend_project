170fe45e464c62e7bb81d13a39b90d46227d0f42
Sambhav Kumar Jha <sambhavjha02003@gmail.com>
2025-08-20 18:52:49 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express, { urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+
+
 const app = express();
 
 app.use(cors(
@@ -22,4 +24,17 @@ app.use(express.urlencoded({
 app.use(express.static("public"))//to store the pdfs , images etc at the server itself
 
 app.use(cookieParser())// to perform crud operations on user's browser
+
+
+//routes import
+
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+// app.get("/api/v1",(req,res)=>{
+//     res.send("hello")
+// })
+app.use("/api/v1/users",userRouter);
+// http://localhost:8000/api/v1/users/register
+
 export { app }
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,11 @@
 // high order fuctions are the functions that can take and read another functions as parameters or arguments and also can return another functions as their return value;
 const asyncHandler = (requestHandler)=> {
-    (req,res,next)=>{
-        Promises.resolve(requestHandler(req,res,next))
+    return (req,res,next)=>{
+        Promise.resolve(requestHandler(req, res, next))
         .catch((err)=>next(err))
     }
 } 
+
 
 export {asyncHandler}
 
